190a06d07a4bc5bb120374429c54265c157e15d1
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:31:11 +0300

    Implement Bird class extending Pet with behaviors

--- Task 7/Bird.java
+++ Task 7/Bird.java
@@ -1,1 +1,25 @@
+public class Bird extends Pet {
 
+    public Bird(String petName) {
+        super(petName);
+    }
+
+    @Override
+    public void eat() {
+        energy += 10;
+        System.out.println(petName + " pecks at its food quickly.");
+    }
+
+    @Override
+    public void play() {
+        int loss = 10 + (int)(Math.random() * 21);
+        energy -= loss;
+        System.out.println(petName + " soars through the sky!");
+    }
+
+    @Override
+    public void sleep() {
+        energy += 30;
+        System.out.println(petName + " is sleeping...");
+    }
+}
