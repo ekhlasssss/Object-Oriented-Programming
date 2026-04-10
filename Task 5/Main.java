0ced7a2652f162d42e4cff32cd7d92223f20a6be
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:19:46 +0300

    Update Main.java

--- Task 5/Main.java
+++ Task 5/Main.java
@@ -1,1 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount("001", "Alice");
+        BankAccount account2 = new BankAccount("002", "Bob");
 
+        account1.deposit(1000);
+        account1.getAccountDetails();
+
+        account1.withdraw(400);
+        account1.getAccountDetails();
+
+        account1.withdraw(700);
+        account1.getAccountDetails();
+
+        account2.deposit(500);
+        account2.getAccountDetails();
+
+        account2.withdraw(200);
+        account2.getAccountDetails();
+    }
+}
