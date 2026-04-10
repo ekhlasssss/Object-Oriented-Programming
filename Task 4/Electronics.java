fcb92f62a67b3ba8ce3877bdd22e8c2ba738cfb3
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:16:41 +0300

    Add Electronics class with warranty information

--- Task 4/Electronics.java
+++ Task 4/Electronics.java
@@ -1,1 +1,12 @@
+public class Electronics extends Product {
+    int warranty;
 
+    public Electronics(String name, double price, int quantity, int warranty) {
+        super(name, price, quantity);
+        this.warranty = warranty;
+    }
+
+    public String getWarrantyInfo() {
+        return "Warranty: " + warranty + " years";
+    }
+}
