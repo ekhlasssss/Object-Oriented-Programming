09eaca28334bb4abad3e2a545a5269d2aa733963
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:30:20 +0300

    Update SimulationManager.java

--- Task 7/SimulationManager.java
+++ Task 7/SimulationManager.java
@@ -1,1 +1,28 @@
+public class SimulationManager {
 
+    public static void startSimulation() {
+        System.out.println("Simulation Started!");
+
+        Dog max = new Dog("Max");
+        Cat luna = new Cat("Luna");
+        Bird sky = new Bird("Sky");
+
+        Pet.displaySimulationStats();
+
+        max.eat();
+        luna.eat();
+        sky.eat();
+
+        max.play();
+        luna.play();
+        sky.play();
+
+        max.sleep();
+
+        String[] conditions = {"Sunny", "Rainy", "Cloudy", "Windy"};
+        String newCondition = conditions[(int)(Math.random() * conditions.length)];
+        Pet.updateEnvironment(newCondition);
+
+        Pet.displaySimulationStats();
+    }
+}
