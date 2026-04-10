3dcd7a778f362b35da295e7197cace764a453002
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:16:13 +0300

    Update Product.java

--- Task 4/Product.java
+++ Task 4/Product.java
@@ -1,1 +1,15 @@
+public class Product {
+    String name;
+    double price;
+    int quantity;
 
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getProductInfo() {
+        return "Product: " + name + ", Price: $" + price + ", Quantity: " + quantity;
+    }
+}
