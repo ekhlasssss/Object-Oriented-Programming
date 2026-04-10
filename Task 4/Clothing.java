547023c9ae43788408443f95265eb64cdb10a06d
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:17:14 +0300

    Add Clothing class extending Product

--- Task 4/Clothing.java
+++ Task 4/Clothing.java
@@ -1,1 +1,14 @@
+public class Clothing extends Product {
+    String size;
+    String material;
 
+    public Clothing(String name, double price, int quantity, String size, String material) {
+        super(name, price, quantity);
+        this.size = size;
+        this.material = material;
+    }
+
+    public String getClothingInfo() {
+        return "Size: " + size + ", Material: " + material;
+    }
+}
