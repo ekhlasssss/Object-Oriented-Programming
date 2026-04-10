65052ddcd68db8e1cb1e459f16db43a1f4394981
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:27:23 +0300

    Update Shape.java

--- Task 6/Shape.java
+++ Task 6/Shape.java
@@ -1,1 +1,16 @@
+public abstract class Shape {
+    protected String name;
 
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+
+    public void displayInfo() {
+        System.out.printf("Shape: %s%n", name);
+        System.out.printf("Area: %.2f%n", calculateArea());
+        System.out.printf("Perimeter: %.2f%n", calculatePerimeter());
+    }
+}
