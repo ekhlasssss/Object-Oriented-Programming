d2573afb15208193f7636ef076af590984d0de81
Ekhlas <ekhlashrayees@gmail.com>
2026-04-10 03:27:08 +0300

    Update Circle.java

--- Task 6/Circle.java
+++ Task 6/Circle.java
@@ -1,1 +1,18 @@
+public class Circle extends Shape {
+    private double radius;
 
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
